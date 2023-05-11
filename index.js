79c54818a716caca937d188450b600ca3af5238b
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-05-10 19:52:15 -0500

    Started writing questions and if statements.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "what is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "provide a description of your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "choose a license added to your repo.",
+        choices: ["Apache License 2.0", "MIT License", "GNU General Public License v3.0", "None"]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
