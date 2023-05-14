68dce2017d930c01b198a956bb995f3d6f7c66dd
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-05-14 11:46:37 -0500

    Finished questions, reorganzied markdown page.

--- index.js
+++ index.js
@@ -1,32 +1,83 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "what is the title of your project?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "provide a description of your project."
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "choose a license added to your repo.",
-        choices: ["Apache License 2.0", "MIT License", "GNU General Public License v3.0", "None"]
-    },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project."
+  },
+  {
+    type: "input",
+    name: "contents",
+    message: "Provide a table of contents of your project if needed."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide what installation is necessary for your project."
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide details about how your project is used."
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license to add to your repo.",
+    choices: ["Apache License 2.0", "MIT License", "GNU General Public License v3.0", "None"]
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Provide names of contributing cohorts as well as links to their GitHub profiles. Include links to any third-party libraries, assets, or tutorials used in your project."
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "Provide additional information about the features of your project."
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide examples of any tests written for your application (if applicable)."
+  },
+  {
+    type: "input",
+    name: "contact",
+    message: "Provide your contact information, including your GitHub username."
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('README file created successfully!');
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const fileName = 'README.md';
+      const data = generateMarkdown(answers);
+      writeToFile(fileName, data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,21 +3,94 @@
 function renderLicenseBadge(license) {
   if (license === "MIT License") {
     return `![MIT License](https://img.shields.io/badge/License-MIT-brightgreen)`
+  } else if (license === "Apache License 2.0") {
+    return `![Apache License 2.0](https://img.shields.io/badge/License-Apache%20License%202.0-brightgreen)`
+  } else if (license === "GNU General Public License v3.0") {
+    return `![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU%20General%20Public%20License%203.0-brightgreen)`
+  } else {
+    return "";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL 3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0.en.html";
+    case "BSD 3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenseLink = renderLicenseLink(license);
+  if (licenseLink) {
+    return `## License
+
+This project is licensed under the [${license}](${licenseLink}) license.`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
 ${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Features](#features)
+- [Tests](#tests)
+- [Contact](#contact)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} license.
+
+## Credits
+
+${data.credits}
+
+## Features
+
+${data.features}
+
+## Tests
+
+${data.tests}
+
+## Contact
+
+${data.contact}
 `;
 }
 
